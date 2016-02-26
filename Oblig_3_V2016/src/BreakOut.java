2206e07475b56f820e2551f0edcb4aac48b7ea73
Kenneth Johan Kristensen <chexxor@outlook.com>
2016-02-26 17:36:01 +0100

    New branch with bricks

--- Oblig_3_V2016/src/BreakOut.java
+++ Oblig_3_V2016/src/BreakOut.java
@@ -1,20 +1,27 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-
+import javafx.scene.shape.Rectangle;
 
 public class BreakOut extends Application{
   @Override 
   public void start(Stage primaryStage) {
-    StackPane pane = new StackPane();
-    
+    Pane pane = new Pane();
+    Rectangle[][] rectangles = new Rectangle[8][8];
+    for(int y = 0; y < 8; y++){
+    	for(int x = 0; x < 8; x++){
+    		rectangles[y][x]= new Rectangle(x*(100+3), y*(20+3), 100, 20);
+    		pane.getChildren().add(rectangles[y][x]);
+    	}
+    }
     
 
     
     Scene scene = new Scene(pane, 950, 750);
-    scene.setFill(Color.BLACK);
+    scene.setFill(Color.RED);
     primaryStage.setTitle("BreakOut"); 
     primaryStage.setScene(scene); 
     primaryStage.show(); 
