c81cc5ad9d089f3c834499368154739dd8c7b076
Allan Arnesen <allan.arnesen@gmail.com>
2016-02-26 19:41:31 +0100

    Added 10 rows and 10 colls with bricks in different colors. Also added the no resizable.

--- Oblig_3_V2016/src/BreakOut.java
+++ Oblig_3_V2016/src/BreakOut.java
@@ -1,22 +1,44 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 
 public class BreakOut extends Application{
   @Override 
   public void start(Stage primaryStage) {
-    StackPane pane = new StackPane();
-    
-    
+	  Pane pane = new Pane();
+	  
+	  
+	  Rectangle[][] rectangles = new Rectangle[10][10];
+	  for(int y = 0; y < 10; y++){
+		  for(int x = 0; x < 10; x++){
+	    	rectangles[y][x]= new Rectangle(x*(100+3), y*(20+3), 100, 20);
+	    	pane.getChildren().add(rectangles[y][x]);
+		  }
+	  }
+	  for(int x = 0; x < 10; x++){
+	    rectangles[0][x].setFill(Color.BLUE);
+	    rectangles[1][x].setFill(Color.BLUE);
+	    rectangles[2][x].setFill(Color.BLUE);
+	    rectangles[3][x].setFill(Color.GREEN);
+	    rectangles[4][x].setFill(Color.GREEN);
+	    rectangles[5][x].setFill(Color.GREEN);
+	    rectangles[6][x].setFill(Color.YELLOW);
+	    rectangles[7][x].setFill(Color.YELLOW);
+	    rectangles[8][x].setFill(Color.YELLOW);
+	    rectangles[9][x].setFill(Color.RED);
+	    
+	    }
 
     
-    Scene scene = new Scene(pane, 950, 750);
+    Scene scene = new Scene(pane, 1030, 830);
     scene.setFill(Color.BLACK);
     primaryStage.setTitle("BreakOut"); 
-    primaryStage.setScene(scene); 
+    primaryStage.setScene(scene);
+    primaryStage.setResizable(false);
     primaryStage.show(); 
   }
 
