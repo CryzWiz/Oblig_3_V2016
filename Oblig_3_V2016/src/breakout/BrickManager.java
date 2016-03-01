6f8dca728532176a2e6f01d14fa3e669929b4507
Benedicte Karlsen <benedictek1@gmail.com>
2016-03-01 11:42:06 +0100

    Moved coloring into separate method, new constructors

--- Oblig_3_V2016/src/breakout/BrickManager.java
+++ Oblig_3_V2016/src/breakout/BrickManager.java
@@ -4,9 +4,20 @@ import javafx.scene.paint.Color;
 
 public class BrickManager implements Settings {
 	Brick[][] bricks;
+	
+	public BrickManager() { 
+		createBricks(BRICK_ROWS, BRICK_COLS);
+		setBrickColors(Color.BLUE, Color.GREEN, Color.YELLOW, Color.RED);
+	}
 
 	public BrickManager(int numberOfRows, int numberOfCols) { 
 		createBricks(numberOfRows, numberOfCols);
+		setBrickColors(Color.BLUE, Color.GREEN, Color.YELLOW, Color.RED);
+
+	}
+	public BrickManager(int numberOfRows, int numberOfCols, Color colorTop, Color colorMid1, Color colorMid2, Color colorBottom) { 
+		createBricks(numberOfRows, numberOfCols);
+		setBrickColors(colorTop, colorMid1, colorMid2, colorBottom);
 
 	}
 	
@@ -19,25 +30,31 @@ public class BrickManager implements Settings {
 	    				y * (BRICK_HEIGHT + BRICK_PADDING_V) + WALL_PADDING_TOP,
 	    				BRICK_WIDTH,
 	    				BRICK_HEIGHT);
-	    		//rect.setFill(BRICK_COLOR);
 	    		
 	    		bricks[y][x]= brick;
-	    		if(y < 3){
-		    		bricks[y][x].setFill(Color.BLUE);
-		    	}
-		    	else if(y < 6){
-		    		bricks[y][x].setFill(Color.GREEN);
-		    	}
-		    	else if(y < 9){
-		    		bricks[y][x].setFill(Color.YELLOW);
-		    	}
-		    	else{
-		    		bricks[y][x].setFill(Color.RED);
-		    	}
 	    	}
-	    	
+	   
 	    }
 	    this.bricks = bricks;
+	}
+	public void setBrickColors(Color colorTop, Color colorMid1, Color colorMid2, Color colorBottom) {
+	    for(int y = 0; y < bricks.length; y++){
+	    	for(int x = 0; x < bricks[y].length; x++){
+	    		if(y < 3){
+		    		bricks[y][x].setFill(colorTop);
+		    	}
+		    	else if(y < 6){
+		    		bricks[y][x].setFill(colorMid1);
+		    	}
+		    	else if(y < 9){
+		    		bricks[y][x].setFill(colorMid2);
+		    	}
+		    	else{
+		    		bricks[y][x].setFill(colorBottom);
+		    	}
+	    		
+	    	}
+	    }
 	}
 	
 
