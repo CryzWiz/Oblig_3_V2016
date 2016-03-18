5ce1c0416600fb88060bbbdb861bc5c01c1f17ed
Benedicte Karlsen <benedictek1@gmail.com>
2016-03-18 17:00:21 +0100

    Fixed lvl 2 and 3

--- Oblig_3_V2016/src/breakout/Level.java
+++ Oblig_3_V2016/src/breakout/Level.java
@@ -5,9 +5,9 @@ import javafx.scene.paint.Color;
 
 
 public enum Level {
-	ONE(1, 99, BRICK_COLORS_LVL1), TWO(2, 15, BRICK_COLORS_LVL2), THREE(3, 10, BRICK_COLORS_LVL3), VICTORY(4, 0,BRICK_COLORS_LVL1);
+	ONE(1, 20, BRICK_COLORS_LVL1), TWO(2, 15, BRICK_COLORS_LVL2), THREE(3, 10, BRICK_COLORS_LVL3), VICTORY(4, 0,BRICK_COLORS_LVL1);
 
-	private static Level level = ONE;
+	private static Level level = TWO;
 	public static Level getLevel(){
 		return level;
 	}
@@ -58,6 +58,37 @@ public enum Level {
 			}
 			break;
 		case TWO:
+			// Diagonal from upper left down to middle (inclusive)
+			if (col < BRICK_COLUMNS / 2 + 1) {
+				if(row - col == 2) {
+					brick.setFill(brickColors[0]);
+					brick.setProtection(true);
+				} else if(row - col == 1 || row == col) {
+					brick.setFill(brickColors[1]);
+					brick.setProtection(true);
+				} else if(col - row == 1) {
+					brick.setFill(brickColors[2]);
+					brick.setProtection(true);
+				} else {
+					brick.setFill(brickColors[3]);
+				}
+			} else {
+			// Diagonal from upper right down to middle (exclusive)
+				if(row + col == 16) {
+					brick.setFill(brickColors[0]);
+					brick.setProtection(true);
+				} else if(row + col == 15 || row + col == 14) {
+					brick.setFill(brickColors[1]);
+					brick.setProtection(true);
+				} else if(row + col == 13) {
+					brick.setFill(brickColors[2]);
+					brick.setProtection(true);
+				} else {
+					brick.setFill(brickColors[3]);
+				}
+			}
+			break;
+		case THREE:
 			if (col < BRICK_COLUMNS / 2) {
 				if (row == col) {
 					brick.setFill(brickColors[0]);
@@ -79,37 +110,6 @@ public enum Level {
 					brick.setFill(brickColors[1]);
 					brick.setProtection(true);
 				} else if (row + col == 12 || row + col == 16) {
-					brick.setFill(brickColors[2]);
-					brick.setProtection(true);
-				} else {
-					brick.setFill(brickColors[3]);
-				}
-			}
-			break;
-		case THREE:
-			// Diagonal from upper left down to middle (inclusive)
-			if (col < BRICK_COLUMNS / 2) {
-				if(row == col) {
-					brick.setFill(brickColors[0]);
-					brick.setProtection(true);
-				} else if(col - row == 1 || col - row == 2) {
-					brick.setFill(brickColors[1]);
-					brick.setProtection(true);
-				} else if(col - row == 3 || col - row == 4) {
-					brick.setFill(brickColors[2]);
-					brick.setProtection(true);
-				} else {
-					brick.setFill(brickColors[3]);
-				}
-			} else {
-			// Diagonal from upper right down to middle (exclusive)
-				if(row + col == BRICK_COLUMNS - 1) {
-					brick.setFill(brickColors[0]);
-					brick.setProtection(true);
-				} else if(row + col == 14 || row + col == 13) {
-					brick.setFill(brickColors[1]);
-					brick.setProtection(true);
-				} else if(row + col == 12 || row + col == 11) {
 					brick.setFill(brickColors[2]);
 					brick.setProtection(true);
 				} else {
