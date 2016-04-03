478880248a94b52bd69308c53ed3eed926eb613e
Benedicte Karlsen <benedictek1@gmail.com>
2016-04-03 21:30:17 +0200

    Changed level to ONE

--- Oblig_3_V2016/src/breakout/Level.java
+++ Oblig_3_V2016/src/breakout/Level.java
@@ -6,7 +6,7 @@ import javafx.scene.paint.Color;
 
 public enum Level {
 	ONE(1, 20, BRICK_COLORS_LVL1), TWO(2, 15, BRICK_COLORS_LVL2), THREE(3, 99, BRICK_COLORS_LVL3), VICTORY(4, 0,BRICK_COLORS_LVL1);
-	private static Level level = THREE;
+	private static Level level = ONE;
 	int value;
 	int removePercentage;
 	Color[] brickColors;
